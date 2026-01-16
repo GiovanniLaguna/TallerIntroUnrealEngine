9f37d9e93dd1dbac335abe95fb1e9eb6182a3b99
GiovanniLaguna <erick.laguna@saei.mx>
2026-01-16 00:05:11 -0600

    SetUp

---
+++ Source/Project_beat_emup/Project_beat_emup.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "Project_beat_emup.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Project_beat_emup, "Project_beat_emup" );
