c202d4e6457c3559d806416f33f50a3049f0d9fb
GiovanniLaguna <erick.laguna@saei.mx>
2026-01-26 09:54:14 -0600

    Update

---
+++ Source/Aura/Public/AuroPlayer.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AuraCharacterBase.h"
+#include "AuroPlayer.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class AURA_API AAuroPlayer : public AAuraCharacterBase
+{
+	GENERATED_BODY()
+	
+};
